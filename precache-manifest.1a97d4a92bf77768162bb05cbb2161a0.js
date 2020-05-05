2af155c693fb600672f0b4812b57d43742886873
Gavin Morris <gavin1997m@gmail.com>
2020-05-05 01:23:15 +0100

    Updates

---
+++ precache-manifest.1a97d4a92bf77768162bb05cbb2161a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07365837f2eda49fa556",
+    "url": "/fypgav/static/css/main.c80dd1a9.chunk.css"
+  },
+  {
+    "revision": "07365837f2eda49fa556",
+    "url": "/fypgav/static/js/main.f6228632.chunk.js"
+  },
+  {
+    "revision": "531bdd15effddcf19570",
+    "url": "/fypgav/static/js/runtime~main.80453cca.js"
+  },
+  {
+    "revision": "76ec76f8f97eba6442a1",
+    "url": "/fypgav/static/css/2.4422e63c.chunk.css"
+  },
+  {
+    "revision": "76ec76f8f97eba6442a1",
+    "url": "/fypgav/static/js/2.ad8de303.chunk.js"
+  },
+  {
+    "revision": "03e0822e6da7a3c9df6e002fc7a9a17a",
+    "url": "/fypgav/index.html"
+  }
+];
